533fb0de135585d05268b23f6fb1185f92221882
Maksym Kondratenko <m.kondratenko.ua@gmail.com>
2025-04-22 16:33:13 +0300

    chore: genrate downloads from url list
    Use a templated url list instead of the changelog to generate downloads
    configuration.

--- scripts/generate_downloads.py
+++ scripts/generate_downloads.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 import argparse
 import logging
-import re
 import sys
 from collections.abc import Sequence
 from textwrap import dedent
@@ -12,36 +11,23 @@ import requests
 from configupdater import ConfigUpdater
 from setup_logging import setup_logging
 
-RELEASE_RE = re.compile(
-    r'^- \[(?P<name>[^\]]+)\]\((?P<url>[^\(]+)\) \(\[checksum\]\([^\)]+\) \/ (?P<sha256>\w{64})\)$',  # noqa: E501
-)
+URL_PATTERNS = {
+    'linux-arm': 'https://get.helm.sh/helm-%s-linux-arm.tar.gz',
+    'linux-arm64': 'https://get.helm.sh/helm-%s-linux-arm64.tar.gz',
+    'linux-riscv64': 'https://get.helm.sh/helm-%s-linux-riscv64.tar.gz',
+    'linux-386': 'https://get.helm.sh/helm-%s-linux-386.tar.gz',
+    'linux-amd64': 'https://get.helm.sh/helm-%s-linux-amd64.tar.gz',
+    'darwin-arm64': 'https://get.helm.sh/helm-%s-darwin-arm64.tar.gz',
+    'darwin-amd64': 'https://get.helm.sh/helm-%s-darwin-amd64.tar.gz',
+    'windows-amd64': 'https://get.helm.sh/helm-%s-windows-amd64.zip',
+}
 
 
-def extract_entries(lines) -> dict[str, dict[str, str]]:
-    result = {}
-    for line in lines:
-        match = RELEASE_RE.match(line)
-        if match:
-            name = match.group('name')
-            url = match.group('url')
-            sha256 = match.group('sha256')
-            result[name] = {
-                'url': url,
-                'sha256': sha256,
-            }
-    return result
-
-
-def get_release_notes(release_tag: str) -> str:
-    response = requests.get(
-        f'https://api.github.com/repos/helm/helm/releases/tags/{release_tag}',
-        verify=True,
-    )
-    json = response.json()
-    body = json['body']
-    if not isinstance(body, str):
-        raise TypeError('Response body must be of type: str')
-    return body
+def get_hash_from_url(url: str) -> str:
+    response = requests.get(url, verify=True)
+    response.raise_for_status()
+    hash = response.text.split()[0]
+    return hash
 
 
 def main(argv: Sequence[str] | None = sys.argv[1:]) -> int:
@@ -71,8 +57,15 @@ def main(argv: Sequence[str] | None = sys.argv[1:]) -> int:
     tag: str | None = args.tag
     version = tag if tag else config_tag
 
-    lines = get_release_notes(version).splitlines()
-    data = extract_entries(lines)
+    data: dict[str, dict[str, str]] = {}
+    for platform, url_pattern in URL_PATTERNS.items():
+        url = url_pattern % version
+        sha256_url = url + '.sha256sum'
+        sha256 = get_hash_from_url(sha256_url)
+        data[platform] = {
+            'url': url,
+            'sha256': sha256,
+        }
 
     download_scripts = dedent(
         f"""
@@ -80,51 +73,51 @@ def main(argv: Sequence[str] | None = sys.argv[1:]) -> int:
         group = helm-binary
         marker = sys_platform == "linux" and platform_machine == "armv6hf"
         marker = sys_platform == "linux" and platform_machine == "armv7l"
-        url = {data["Linux arm"]["url"]}
-        sha256 = {data["Linux arm"]["sha256"]}
+        url = {data["linux-arm"]["url"]}
+        sha256 = {data["linux-arm"]["sha256"]}
         extract = tar
         extract_path = linux-arm/helm
         [helm]
         group = helm-binary
         marker = sys_platform == "linux" and platform_machine == "aarch64"
-        url = {data["Linux arm64"]["url"]}
-        sha256 = {data["Linux arm64"]["sha256"]}
+        url = {data["linux-arm64"]["url"]}
+        sha256 = {data["linux-arm64"]["sha256"]}
         extract = tar
         extract_path = linux-arm64/helm
         [helm]
         group = helm-binary
         marker = sys_platform == "linux" and platform_machine == "riscv64"
-        url = {data["Linux riscv64"]["url"]}
-        sha256 = {data["Linux riscv64"]["sha256"]}
+        url = {data["linux-riscv64"]["url"]}
+        sha256 = {data["linux-riscv64"]["sha256"]}
         extract = tar
         extract_path = linux-riscv64/helm
         [helm]
         group = helm-binary
         marker = sys_platform == "linux" and platform_machine == "i386"
         marker = sys_platform == "linux" and platform_machine == "i686"
-        url = {data["Linux i386"]["url"]}
-        sha256 = {data["Linux i386"]["sha256"]}
+        url = {data["linux-386"]["url"]}
+        sha256 = {data["linux-386"]["sha256"]}
         extract = tar
         extract_path = linux-386/helm
         [helm]
         group = helm-binary
         marker = sys_platform == "linux" and platform_machine == "x86_64"
-        url = {data["Linux amd64"]["url"]}
-        sha256 = {data["Linux amd64"]["sha256"]}
+        url = {data["linux-amd64"]["url"]}
+        sha256 = {data["linux-amd64"]["sha256"]}
         extract = tar
         extract_path = linux-amd64/helm
         [helm]
         group = helm-binary
         marker = sys_platform == "darwin" and platform_machine == "arm64"
-        url = {data["MacOS arm64"]["url"]}
-        sha256 = {data["MacOS arm64"]["sha256"]}
+        url = {data["darwin-arm64"]["url"]}
+        sha256 = {data["darwin-arm64"]["sha256"]}
         extract = tar
         extract_path = darwin-arm64/helm
         [helm]
         group = helm-binary
         marker = sys_platform == "darwin" and platform_machine == "x86_64"
-        url = {data["MacOS amd64"]["url"]}
-        sha256 = {data["MacOS amd64"]["sha256"]}
+        url = {data["darwin-amd64"]["url"]}
+        sha256 = {data["darwin-amd64"]["sha256"]}
         extract = tar
         extract_path = darwin-amd64/helm
         [helm.exe]
@@ -132,8 +125,8 @@ def main(argv: Sequence[str] | None = sys.argv[1:]) -> int:
         marker = sys_platform == "win32" and platform_machine == "AMD64"
         marker = sys_platform == "win32" and platform_machine == "ARM64"
         marker = sys_platform == "cygwin" and platform_machine == "x86_64"
-        url = {data["Windows amd64"]["url"]}
-        sha256 = {data["Windows amd64"]["sha256"]}
+        url = {data["windows-amd64"]["url"]}
+        sha256 = {data["windows-amd64"]["sha256"]}
         extract = zip
         extract_path = windows-amd64/helm.exe
         """,

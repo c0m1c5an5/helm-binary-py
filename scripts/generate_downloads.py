fd81b6b4f1f7f15e19a687ccc69f4d96bcb11944
Maksym Kondratenko <m.kondratenko.ua@gmail.com>
2025-04-22 15:33:06 +0300

    chore: update setup.cfg with generate_downloads script
    Update generate_downloads script to directly change setup.cfg file.

--- scripts/generate_downloads.py
+++ scripts/generate_downloads.py
@@ -6,20 +6,15 @@ import logging
 import re
 import sys
 from collections.abc import Sequence
+from textwrap import dedent
 
 import requests
+from configupdater import ConfigUpdater
 from setup_logging import setup_logging
-from setuptools.config.setupcfg import read_configuration
-
 
 RELEASE_RE = re.compile(
     r'^- \[(?P<name>[^\]]+)\]\((?P<url>[^\(]+)\) \(\[checksum\]\([^\)]+\) \/ (?P<sha256>\w{64})\)$',  # noqa: E501
 )
-
-
-def version_from_setup_cfg():
-    version: str = read_configuration('setup.cfg')['metadata']['version']
-    return 'v' + version.removeprefix('v').split('-')[0]
 
 
 def extract_entries(lines) -> dict[str, dict[str, str]]:
@@ -64,76 +59,90 @@ def main(argv: Sequence[str] | None = sys.argv[1:]) -> int:
     args = parser.parse_args(argv)
     logger.debug('Args: %s', args.__dict__)
 
+    config = ConfigUpdater()
+    config.read('setup.cfg')
+
+    config_version = config['metadata']['version'].value
+    if not isinstance(config_version, str):
+        raise ValueError('Metadata version must be of type: str')
+
+    config_tag = 'v' + config_version.removeprefix('v').split('-')[0]
+
     tag: str | None = args.tag
-    version = tag if tag else version_from_setup_cfg()
+    version = tag if tag else config_tag
 
     lines = get_release_notes(version).splitlines()
     data = extract_entries(lines)
 
-    print(
+    download_scripts = dedent(
         f"""
-    [helm]
-    group = helm-binary
-    marker = sys_platform == "linux" and platform_machine == "armv6hf"
-    marker = sys_platform == "linux" and platform_machine == "armv7l"
-    url = {data["Linux arm"]["url"]}
-    sha256 = {data["Linux arm"]["sha256"]}
-    extract = tar
-    extract_path = linux-arm/helm
-    [helm]
-    group = helm-binary
-    marker = sys_platform == "linux" and platform_machine == "aarch64"
-    url = {data["Linux arm64"]["url"]}
-    sha256 = {data["Linux arm64"]["sha256"]}
-    extract = tar
-    extract_path = linux-arm64/helm
-    [helm]
-    group = helm-binary
-    marker = sys_platform == "linux" and platform_machine == "riscv64"
-    url = {data["Linux riscv64"]["url"]}
-    sha256 = {data["Linux riscv64"]["sha256"]}
-    extract = tar
-    extract_path = linux-riscv64/helm
-    [helm]
-    group = helm-binary
-    marker = sys_platform == "linux" and platform_machine == "i386"
-    marker = sys_platform == "linux" and platform_machine == "i686"
-    url = {data["Linux i386"]["url"]}
-    sha256 = {data["Linux i386"]["sha256"]}
-    extract = tar
-    extract_path = linux-386/helm
-    [helm]
-    group = helm-binary
-    marker = sys_platform == "linux" and platform_machine == "x86_64"
-    url = {data["Linux amd64"]["url"]}
-    sha256 = {data["Linux amd64"]["sha256"]}
-    extract = tar
-    extract_path = linux-amd64/helm
-    [helm]
-    group = helm-binary
-    marker = sys_platform == "darwin" and platform_machine == "arm64"
-    url = {data["MacOS arm64"]["url"]}
-    sha256 = {data["MacOS arm64"]["sha256"]}
-    extract = tar
-    extract_path = darwin-arm64/helm
-    [helm]
-    group = helm-binary
-    marker = sys_platform == "darwin" and platform_machine == "x86_64"
-    url = {data["MacOS amd64"]["url"]}
-    sha256 = {data["MacOS amd64"]["sha256"]}
-    extract = tar
-    extract_path = darwin-amd64/helm
-    [helm.exe]
-    group = helm-binary
-    marker = sys_platform == "win32" and platform_machine == "AMD64"
-    marker = sys_platform == "win32" and platform_machine == "ARM64"
-    marker = sys_platform == "cygwin" and platform_machine == "x86_64"
-    url = {data["Windows amd64"]["url"]}
-    sha256 = {data["Windows amd64"]["sha256"]}
-    extract = zip
-    extract_path = windows-amd64/helm.exe
-    """,
+        [helm]
+        group = helm-binary
+        marker = sys_platform == "linux" and platform_machine == "armv6hf"
+        marker = sys_platform == "linux" and platform_machine == "armv7l"
+        url = {data["Linux arm"]["url"]}
+        sha256 = {data["Linux arm"]["sha256"]}
+        extract = tar
+        extract_path = linux-arm/helm
+        [helm]
+        group = helm-binary
+        marker = sys_platform == "linux" and platform_machine == "aarch64"
+        url = {data["Linux arm64"]["url"]}
+        sha256 = {data["Linux arm64"]["sha256"]}
+        extract = tar
+        extract_path = linux-arm64/helm
+        [helm]
+        group = helm-binary
+        marker = sys_platform == "linux" and platform_machine == "riscv64"
+        url = {data["Linux riscv64"]["url"]}
+        sha256 = {data["Linux riscv64"]["sha256"]}
+        extract = tar
+        extract_path = linux-riscv64/helm
+        [helm]
+        group = helm-binary
+        marker = sys_platform == "linux" and platform_machine == "i386"
+        marker = sys_platform == "linux" and platform_machine == "i686"
+        url = {data["Linux i386"]["url"]}
+        sha256 = {data["Linux i386"]["sha256"]}
+        extract = tar
+        extract_path = linux-386/helm
+        [helm]
+        group = helm-binary
+        marker = sys_platform == "linux" and platform_machine == "x86_64"
+        url = {data["Linux amd64"]["url"]}
+        sha256 = {data["Linux amd64"]["sha256"]}
+        extract = tar
+        extract_path = linux-amd64/helm
+        [helm]
+        group = helm-binary
+        marker = sys_platform == "darwin" and platform_machine == "arm64"
+        url = {data["MacOS arm64"]["url"]}
+        sha256 = {data["MacOS arm64"]["sha256"]}
+        extract = tar
+        extract_path = darwin-arm64/helm
+        [helm]
+        group = helm-binary
+        marker = sys_platform == "darwin" and platform_machine == "x86_64"
+        url = {data["MacOS amd64"]["url"]}
+        sha256 = {data["MacOS amd64"]["sha256"]}
+        extract = tar
+        extract_path = darwin-amd64/helm
+        [helm.exe]
+        group = helm-binary
+        marker = sys_platform == "win32" and platform_machine == "AMD64"
+        marker = sys_platform == "win32" and platform_machine == "ARM64"
+        marker = sys_platform == "cygwin" and platform_machine == "x86_64"
+        url = {data["Windows amd64"]["url"]}
+        sha256 = {data["Windows amd64"]["sha256"]}
+        extract = zip
+        extract_path = windows-amd64/helm.exe
+        """,
+    ).strip()
+
+    config['setuptools_download']['download_scripts'].set_values(
+        download_scripts.splitlines(),
     )
+    config.update_file()
 
     return 0
 
